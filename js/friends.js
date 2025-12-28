fc09de6917180425b9c42e7d20c51aab5dbbb51d
Mr. Coder <chbibhadi1014@gmail.com>
2025-12-28 14:02:32 -0500

    Update friends.js

--- js/friends.js
+++ js/friends.js
@@ -1,1 +1,14 @@
+import { db } from "./firebase.js";
+import { collection, query, where, getDocs, doc, updateDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+export async function addFriend(code) {
+  const q = query(collection(db, "users"), where("friendCode", "==", code));
+  const snap = await getDocs(q);
+  if (snap.empty) return alert("Friend not found");
+  
+  const friendId = snap.docs[0].id;
+  const uid = localStorage.getItem("uid");
+  
+  await updateDoc(doc(db, "users", uid), { friends: arrayUnion(friendId) });
+  alert("Friend added!");
+}
