8ed71d04d95f64b4755995a7a3066f5b7df9e8e2
Mr. Coder <chbibhadi1014@gmail.com>
2025-12-28 13:15:13 -0500

    Update tic-tac-toe.js

--- games/tic-tac-toe.js
+++ games/tic-tac-toe.js
@@ -1,1 +1,17 @@
+window.startGame = (g) => {
+  if (g !== "tic-tac-toe") return;
+  let board = Array(9).fill("");
+  const el = document.getElementById("game");
 
+  function draw() {
+    el.innerHTML = board.map((c,i)=>`<button onclick="move(${i})">${c}</button>`).join("");
+  }
+
+  window.move = i => {
+    if (board[i]) return;
+    board[i] = "X";
+    draw();
+  };
+
+  draw();
+};
