e0a74bb0d0790b1723ff5748f3ae96fd3d24c6fc
Mr. Coder <chbibhadi1014@gmail.com>
2025-12-28 13:14:56 -0500

    Update guess-number.js

--- games/guess-number.js
+++ games/guess-number.js
@@ -1,1 +1,7 @@
-
+window.startGame = (g) => {
+  if (g !== "guess-number") return;
+  document.getElementById("game").innerHTML = `
+    <h3>Guess the Number</h3>
+    <input id="n"><button onclick="alert('Guess sent')">Guess</button>
+  `;
+};
