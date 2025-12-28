f9f5d6b92b148ccc0027d35034912861c84b68de
Mr. Coder <chbibhadi1014@gmail.com>
2025-12-28 14:02:13 -0500

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -1,1 +1,16 @@
+import { db, auth } from "./firebase.js";
+import { doc, updateDoc, onSnapshot, arrayUnion } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+const roomId = localStorage.getItem("room");
+const chatRef = doc(db, "rooms", roomId);
+
+onSnapshot(chatRef, snap => {
+  const chatEl = document.getElementById("chat");
+  chatEl.innerHTML = snap.data().chat.map(c => `<b>${c.uid}</b>: ${c.message}`).join("<br>");
+});
+
+window.sendMessage = async (msg) => {
+  await updateDoc(chatRef, {
+    chat: arrayUnion({ uid: auth.currentUser.uid, message: msg, timestamp: Date.now() })
+  });
+};
