e33874e4d3acc68397b4c64c4d7a218b52ab6147
Mr. Coder <chbibhadi1014@gmail.com>
2025-12-28 13:12:00 -0500

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,23 @@
+import { auth, db } from "./firebase.js";
+import { signInAnonymously } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+export async function login(username) {
+  const cred = await signInAnonymously(auth);
+  const uid = cred.user.uid;
+
+  const userRef = doc(db, "users", uid);
+  const snap = await getDoc(userRef);
+
+  if (!snap.exists()) {
+    const friendCode = Math.random().toString(36).substring(2, 8).toUpperCase();
+    await setDoc(userRef, {
+      username,
+      friendCode,
+      friends: []
+    });
+  }
+
+  localStorage.setItem("uid", uid);
+  window.location.href = "lobby.html";
+}
