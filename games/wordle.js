a421acd39bcc7323021a7f43bfd3bc204ac2fda5
Mr. Coder <chbibhadi1014@gmail.com>
2025-12-28 13:15:38 -0500

    Update wordle.js

--- games/wordle.js
+++ games/wordle.js
@@ -1,1 +1,9 @@
-
+window.startGame = (g) => {
+  if (g !== "wordle") return;
+  const word = "APPLE";
+  document.getElementById("game").innerHTML = `
+    <h3>Wordle</h3>
+    <input id="w">
+    <button onclick="alert('Checked')">Submit</button>
+  `;
+};
