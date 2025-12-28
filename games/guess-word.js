fac3cdec152076c705a3f5b368a3f370edbb4719
Mr. Coder <chbibhadi1014@gmail.com>
2025-12-28 13:15:59 -0500

    Update guess-word.js

--- games/guess-word.js
+++ games/guess-word.js
@@ -1,1 +1,8 @@
-
+window.startGame = (g) => {
+  if (g !== "guess-word") return;
+  document.getElementById("game").innerHTML = `
+    <h3>Guess the Word</h3>
+    <input placeholder="Guess">
+    <button>Submit</button>
+  `;
+};
